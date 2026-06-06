fe62c89cca35269e8911e1ddcdb5ba36805ce4b8
Harish Raj <harish20060926@gmail.com>
2026-06-06 10:21:01 +0530

    Created project structure and expense model

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-print("hello expense tracker")
+ #print("hello expense tracker")
+
+expense =[]
+
+amt =int(input("enter amount spent:"))
+category =input("enter category of expense:")
+
+print("AMOUNT:",amt)
+print("CATEGORY:",category)
+
+expenses={
+    "amount":amt,
+    "category":category
+}
+
+expense.append(expenses)
+print("EXPENSES:",expense)
